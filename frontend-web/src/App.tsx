975cb4ee5e601b3759a1d2ef1b1aa02b2836e47c
Sumaiya Sultana <sumaiya.sultana.sinthy@gmail.com>
2024-03-24 12:49:50 +0100

    #7 Request session on app load

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CsvUploadForm } from "./components/CsvUploadForm";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("/api/get-session")
+      .catch(() => window.alert("Unable to set session. Please refresh!"));
+  }, []);
+
   return (
     <div>
       <CsvUploadForm />
--- frontend-web/src/main.tsx
+++ frontend-web/src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -7,8 +6,4 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
