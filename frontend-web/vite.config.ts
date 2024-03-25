979d5fca8713f86729f0b932631e3f2eabbd6394
Sumaiya Sultana <sumaiya.sultana.sinthy@gmail.com>
2024-03-25 22:19:56 +0100

    #27 Scalable

--- frontend-web/vite.config.ts
+++ frontend-web/vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import 'dotenv/config'
 
+// @ts-ignore
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      // @ts-ignore
+      '/api': process.env.VITE_GW_SERVER,
     }
   }
 })
